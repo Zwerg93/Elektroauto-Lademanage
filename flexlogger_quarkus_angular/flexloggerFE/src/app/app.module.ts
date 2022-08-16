78d436ea44e12adce511ae9216a8e62e0f40e076
Marcel Pouget <mapouget15160@gmail.com>
2022-08-16 13:39:25 +0200

    commit

--- flexlogger_quarkus_angular/flexloggerFE/src/app/app.module.ts
+++ flexlogger_quarkus_angular/flexloggerFE/src/app/app.module.ts
@@ -1,10 +1,12 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgxChartsModule} from "@swimlane/ngx-charts";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChartComponent } from './chart/chart.component';
-import { LoginComponent } from './login/login.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { LoginComponent } from './components/login/login.component';
 import {GoogleChartsModule} from "angular-google-charts";
 
 @NgModule({
@@ -17,6 +19,8 @@ import {GoogleChartsModule} from "angular-google-charts";
     BrowserModule,
     AppRoutingModule,
     GoogleChartsModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
